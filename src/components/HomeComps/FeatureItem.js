7a7c5892d8176e6c24c876ffcdfb3c1f9507fcf7
NarwhalG <rtmoddingteam@gmail.com>
2022-04-24 18:25:05 -0700

    simplification
    more useful and less ugly to look at.

--- src/components/HomeComps/FeatureItem.js
+++ src/components/HomeComps/FeatureItem.js
@@ -1,15 +1,15 @@
 function FeatureItem(props) {
 	const { icon, title, color } = props;
+	var classes = ["item"];
+	if (props.fancy) classes.push("fancyCard");
+	if (props.padded) classes.push("paddedItem");
+	if (!props.noback) classes.push("themeBackMid");
 	return (
 		<div
-			className={
-				"item themeBackMid " +
-				(props.fancy ? "fancyCard" : "") +
-				` ${props.padded ? "paddedItem" : ""}`
-			}
+			className={classes.join(" ")}
 			style={{ paddingTop: props.padTop ? "10px" : null }}
 		>
-			<img src={icon} className="lightenedDarkness themeIcon hmargin" />
+			{icon && <img src={icon} className="themeIcon hmargin" />}
 			<h2 style={{ color: color || null }}>{title}</h2>
 			{props.children}
 		</div>
